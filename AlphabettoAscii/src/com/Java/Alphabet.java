839caf8085b4b818afb5b3a2ac958f7577f96d73
AishwaryaHalakatti <89127428+AishwaryaHalakatti@users.noreply.github.com>
2024-04-01 15:04:34 +0530

    Add files via upload

---
+++ AlphabettoAscii/src/com/Java/Alphabet.java
@@ -0,0 +1,12 @@
+
+package com.Java;
+
+public class Alphabet {
+
+    public static void main(String[] args) {
+        for (int i = 65; i <= 90; i++) {
+            System.out.println(" The ASCII value of " + (char) i + "  =  " + i);
+        }
+    }
+
+}
